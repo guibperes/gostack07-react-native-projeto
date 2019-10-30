7e3aaf163bc7e4d20ce790fa539d6f7c1b28d0a0
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-10-30 15:09:16 -0300

    finished api get data and list styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Keyboard } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { api } from '../../services/api'
 
 import {
   Container,
   Form,
   Input,
-  SubmitButton
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButtton,
+  ProfileButtonText
 } from './styles'
 
 export function Main () {
+  const [newUser, setNewUser] = useState('')
+  const [users, setUsers] = useState([])
+
+  async function handleSubmit () {
+    const { data: apiData } = await api.get(`/users/${newUser}`)
+
+    const data = {
+      name: apiData.name,
+      login: apiData.login,
+      bio: apiData.bio,
+      avatar: apiData.avatar_url
+    }
+
+    setUsers([...users, data])
+    setNewUser('')
+    Keyboard.dismiss()
+  }
+
   return (
     <Container>
       <Form>
@@ -16,11 +44,31 @@ export function Main () {
           autoCorrect={false}
           autoCapitalize='none'
           placeholder='Adicionar Usuário'
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType='send'
+          onSubmitEditing={handleSubmit}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleSubmit}>
           <Icon name='add' size={20} color='#FFF' />
         </SubmitButton>
       </Form>
+
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+
+            <ProfileButtton>
+              <ProfileButtonText>Ver perfil</ProfileButtonText>
+            </ProfileButtton>
+          </User>
+        )}
+      />
     </Container>
   )
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -32,3 +32,53 @@ export const SubmitButton = styled(RectButton)`
   margin-left: 10px;
   padding: 0 12px;
 `
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false
+})`
+  margin-top: 20px;
+`
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  background: #EEE;
+`
+
+export const Name = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
+  text-align: center;
+`
+
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2
+})`
+  font-size: 13px;
+  line-height: 18px;
+  color: #999;
+  text-align: center;
+`
+
+export const ProfileButtton = styled(RectButton)`
+  margin-top: 10px;
+  align-self: stretch;
+  border-radius: 4px;
+  background: #7159C1;
+  justify-content: center;
+  align-items: center;
+  height: 36px;
+`
+
+export const ProfileButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #FFF;
+`
