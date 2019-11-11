243bcd5ffb791624ea6fcd2674b86ed0b0088365
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-11-11 13:41:04 -0300

    added navigation bar white color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 import { Routes } from './routes';
 
 export function App() {
+  useEffect(() => {
+    changeNavigationBarColor('#FFFFFF', true);
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor='#7159C1' barStyle='light-content' />
